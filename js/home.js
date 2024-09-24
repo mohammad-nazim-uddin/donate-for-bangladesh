83ac19aea82e2b3b29fa945ad77d4294528dbe5b
Nazim <nazimuddinasif.1@gmail.com>
2024-09-24 19:45:21 +0600

    button functionality for feni flood

--- js/home.js
+++ js/home.js
@@ -1,4 +1,5 @@
 // noakhali flood
+
 document.getElementById('donate-btn').addEventListener('click', function () {
     const donateAmount = getInputFieldById('input-field')
     const currentAmount = getTextFieldById('current-amount')
@@ -24,6 +25,50 @@ document.getElementById('donate-btn').addEventListener('click', function () {
 
     const p = document.createElement('p');
     p.innerText = `${donateAmount} Taka Donated for flood in Noakhali`;
+    p.classList.add('text-xl','font-bold')
+
+    document.getElementById('donation-history').appendChild(p)
+
+    const now = new Date();
+    const date = now.toLocaleDateString();
+    const time = now.toLocaleTimeString();
+
+    const donationEntry = `Donation made on ${date} at ${time} (standard time Bangladesh)`;
+
+    const historyList = document.getElementById("donation-history");
+    const listItem = document.createElement("p");
+    listItem.textContent = donationEntry;
+    historyList.appendChild(listItem);
+
+
+})
+
+// feni flood
+document.getElementById('donate-btn-feni').addEventListener('click', function () {
+    const donateAmount = getInputFieldById('input-field-feni')
+    const currentAmount = getTextFieldById('current-amount')
+
+    const remainingAmount = getTextFieldById('remaining-amount')
+
+    if (isNaN(donateAmount) || donateAmount <= 0) {
+        alert('Please enter a valid positive number.');
+        return;
+    }
+
+    const newAmount = donateAmount + currentAmount
+
+    const newRemainingAmount = remainingAmount - newAmount
+
+    document.getElementById('current-amount').innerText = newAmount
+
+    document.getElementById('input-field-feni').value = ''
+
+    document.getElementById('remaining-amount').innerText = newRemainingAmount
+
+    alert(`Your money added to the fund`)
+
+    const p = document.createElement('p');
+    p.innerText = `${donateAmount} Taka Donated for flood in Feni`;
     p.classList.add('text-xl','font-bold')
 
     document.getElementById('donation-history').appendChild(p)
