29bf97971945dfea3ff5b9bf91df23645ceef5d3
Nazim <nazimuddinasif.1@gmail.com>
2024-09-24 15:12:49 +0600

    validation process checked

--- js/home.js
+++ js/home.js
@@ -1,17 +1,24 @@
-document.getElementById('donate-btn').addEventListener('click',function(){
+document.getElementById('donate-btn').addEventListener('click', function () {
     const donateAmount = getInputFieldById('input-field')
     const currentAmount = getTextFieldById('current-amount')
 
     const remainingAmount = getTextFieldById('remaining-amount')
+
+    if (isNaN(donateAmount) || donateAmount <= 0) {
+        alert('Please enter a valid positive number.');
+        return;
+    }
 
     const newAmount = donateAmount + currentAmount
 
     const newRemainingAmount = remainingAmount - newAmount
 
     document.getElementById('current-amount').innerText = newAmount
-    
+
     document.getElementById('input-field').value = ''
 
     document.getElementById('remaining-amount').innerText = newRemainingAmount
+
+
 })
 
