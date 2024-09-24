13e83f138dd8dd232e7e47fd4cdf27689bbdd919
Nazim <nazimuddinasif.1@gmail.com>
2024-09-24 09:12:56 +0600

    first button functionality added

--- js/home.js
+++ js/home.js
@@ -1,3 +1,14 @@
 document.getElementById('donate-btn').addEventListener('click',function(){
-    console.log('click lagse')
+    const donateAmount = document.getElementById('input-field').value;
+    const currentAmount = document.getElementById('current-amount').innerText;
+    const donateAmountNumber = parseFloat(donateAmount)
+    const currentAmountNumber = parseFloat(currentAmount)
+
+    const newAmount = donateAmountNumber + currentAmountNumber
+
+    document.getElementById('current-amount').innerText = newAmount
+    
+    document.getElementById('input-field').value = ''
+    
+    
 })
